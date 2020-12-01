a8ad3f6378f7de3d1b1bce6dc9f155f3e80d59e0
JohnOGram <john@minns.io>
2020-12-01 20:14:27 +0000

    Bootstrap basic SPA

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,68 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
+import { ReportRepair } from './Components/01-ReportRepair/ReportRepair';
 
-function App() {
+interface AOCComponent {
+  text: string;
+  route: string;
+  component: JSX.Element;
+}
+
+const components: AOCComponent[] = [
+  {
+    text: "01 - Report Repair",
+    route: "/report-repair",
+    component: <ReportRepair />
+  }
+];
+
+function renderNav(): JSX.Element {
   return (
-    <div className="App">
-      <h1>Hello, World!</h1>
-    </div>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        {components.map(component => (
+          <li>
+            <Link to={component.route}>{component.text}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+function renderDefaultRoute(): JSX.Element {
+  return (
+    <Route path="/">
+      <h1>Advent of Code 2020</h1>
+      <a href="https://github.com/Johnogram/aoc2020/" target="_blank">https://github.com/Johnogram/aoc2020/</a>
+    </Route>
+  );
+}
+
+function renderSwitch(): JSX.Element {
+  return (
+    <Switch>
+      {components.map(component => (
+        <Route path={component.route}>
+          {component.component}
+        </Route>
+      ))}
+      {renderDefaultRoute()}
+    </Switch>
+  );
+}
+
+function App(): JSX.Element {
+  return (
+    <Router>
+      <div>
+        {renderNav()}
+        {renderSwitch()}
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Components/01-ReportRepair/ReportRepair.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function ReportRepair(): JSX.Element {
+    return (
+        <h1>Report Repair</h1>
+    );
+}
